fc22d0404fe3da61c2f1efec032cfea3277cca1b
mdk <578562554@qq.com>
2026-02-07 22:51:39 +0800

    feat: switch to header body footer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import { useEffect, useState, type CSSProperties } from "react";
+import { useEffect, useState } from "react";
 import {
   IonApp,
-  IonCard,
-  IonCardContent,
-  IonCardHeader,
-  IonCardSubtitle,
-  IonCardTitle,
   IonContent,
-  IonCol,
-  IonGrid,
+  IonFooter,
+  IonHeader,
   IonPage,
-  IonRow,
+  IonTitle,
   IonText,
+  IonToolbar,
 } from "@ionic/react";
 import { incrementOpenCount, subscribeConfig } from "./storage/settings";
-
-const contentStyle = {
-  "--background": "#f7f7fb",
-} as CSSProperties;
 
 export default function App() {
   const [openCount, setOpenCount] = useState(0);
@@ -41,28 +33,21 @@ export default function App() {
   return (
     <IonApp>
       <IonPage>
-        <IonContent fullscreen className="ion-padding" style={contentStyle}>
-          <IonGrid style={{ minHeight: "100%" }}>
-            <IonRow
-              className="ion-justify-content-center ion-align-items-center"
-              style={{ minHeight: "100%" }}
-            >
-              <IonCol size="12" sizeMd="8" sizeLg="6" style={{ maxWidth: 720 }}>
-                <IonCard>
-                  <IonCardHeader className="ion-text-center">
-                    <IonCardTitle>Hello World</IonCardTitle>
-                    <IonCardSubtitle>
-                      React + TypeScript + Vite + Ionic React
-                    </IonCardSubtitle>
-                  </IonCardHeader>
-                  <IonCardContent className="ion-text-center">
-                    <IonText color="medium">Open count: {openCount}</IonText>
-                  </IonCardContent>
-                </IonCard>
-              </IonCol>
-            </IonRow>
-          </IonGrid>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Hello World</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonText>React + TypeScript + Vite + Ionic React</IonText>
         </IonContent>
+        <IonFooter>
+          <IonToolbar>
+            <IonText className="ion-padding-start">
+              Open count: {openCount}
+            </IonText>
+          </IonToolbar>
+        </IonFooter>
       </IonPage>
     </IonApp>
   );
