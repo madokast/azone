2b38228bfb7b9280170856fbb6993e2af65ab9f2
mdk <578562554@qq.com>
2026-02-20 00:56:22 +0800

    简化首页布局，只显示List组件
    - 删除首页的标题和描述信息
    - 移除外层的padding div
    - 只保留List组件显示最新动态
    - 保持原有的Post卡片显示和点击跳转功能

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,29 +3,22 @@ import { List } from 'antd-mobile';
 
 export default function Home() {
   return (
-    <div style={{ padding: 16 }}>
-      <h1>首页</h1>
-      <p>React + TypeScript + Vite + Ant Design Mobile</p>
-      
-      <div style={{ marginTop: 24 }}>
-        <List header='最新动态'>
-          <List.Item>
-            <Post id="1" articleMode={false} />
-          </List.Item>
-          <List.Item>
-            <Post id="2" articleMode={false} />
-          </List.Item>
-          <List.Item>
-            <Post id="3" articleMode={false} />
-          </List.Item>
-          <List.Item>
-            <Post id="4" articleMode={false} />
-          </List.Item>
-          <List.Item>
-            <Post id="5" articleMode={false} />
-          </List.Item>
-        </List>
-      </div>
-    </div>
+    <List header='最新动态'>
+      <List.Item>
+        <Post id="1" articleMode={false} />
+      </List.Item>
+      <List.Item>
+        <Post id="2" articleMode={false} />
+      </List.Item>
+      <List.Item>
+        <Post id="3" articleMode={false} />
+      </List.Item>
+      <List.Item>
+        <Post id="4" articleMode={false} />
+      </List.Item>
+      <List.Item>
+        <Post id="5" articleMode={false} />
+      </List.Item>
+    </List>
   );
 }
