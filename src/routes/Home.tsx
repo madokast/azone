8ffe3318c8d48c13ea39563d8ff3416ff0a888a7
mdk <578562554@qq.com>
2026-02-20 00:54:24 +0800

    优化首页布局，使用List组件显示多条Post
    - 修改Home组件，使用antd-mobile的List组件
    - 添加List.header为'最新动态'
    - 在List中显示5条Post
    - 每条Post都设置articleMode={false}，以卡片形式显示
    - 保持原有的点击跳转功能

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,5 @@
 import Post from './Post';
+import { List } from 'antd-mobile';
 
 export default function Home() {
   return (
@@ -7,7 +8,23 @@ export default function Home() {
       <p>React + TypeScript + Vite + Ant Design Mobile</p>
       
       <div style={{ marginTop: 24 }}>
-        <Post id="1" articleMode={false} />
+        <List header='最新动态'>
+          <List.Item>
+            <Post id="1" articleMode={false} />
+          </List.Item>
+          <List.Item>
+            <Post id="2" articleMode={false} />
+          </List.Item>
+          <List.Item>
+            <Post id="3" articleMode={false} />
+          </List.Item>
+          <List.Item>
+            <Post id="4" articleMode={false} />
+          </List.Item>
+          <List.Item>
+            <Post id="5" articleMode={false} />
+          </List.Item>
+        </List>
       </div>
     </div>
   );
