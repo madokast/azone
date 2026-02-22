9048c7431431f2f7a3eeb8557fa66ac4b06c4805
mdk <578562554@qq.com>
2026-02-22 19:15:17 +0800

    添加 ConfigProvider 并设置英文语言

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ConfigProvider } from 'antd-mobile'
 import "antd-mobile/es/global";
 import "./styles/theme-tokens.css";
+import enUS from 'antd-mobile/es/locales/en-US'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={enUS}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
