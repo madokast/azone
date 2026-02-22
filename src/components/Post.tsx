12a060c98131cac8de92ac2afc21537362ecab35
mdk <578562554@qq.com>
2026-02-22 19:24:37 +0800

    为 Post 组件添加点击展开功能，点击后显示完整内容，再次点击无效果

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { type Post as PostType } from '../storage/posts';
 
 interface PostProps {
@@ -5,18 +6,30 @@ interface PostProps {
 }
 
 export default function Post({ post }: PostProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleClick = () => {
+    if (!expanded) {
+      setExpanded(true);
+    }
+  };
+
   return (
     <div style={{ margin: 0 }}>
       <p 
+        onClick={handleClick}
         style={{ 
           fontSize: 14, 
           lineHeight: 1.4, 
           marginBottom: 8,
-          display: '-webkit-box',
-          WebkitLineClamp: 2,
-          WebkitBoxOrient: 'vertical',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis'
+          cursor: 'pointer',
+          ...(!expanded && {
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis'
+          })
         }}
       >
         {post.content}
