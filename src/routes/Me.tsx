07297a1852863bf040c6d8a8118eafd31719e9fa
mdk <578562554@qq.com>
2026-03-07 00:44:17 +0800

    refactor(Me): improve S3 config form UI and error handling

--- src/routes/Me.tsx
+++ src/routes/Me.tsx
@@ -1,4 +1,4 @@
-import { Divider, Form, Segmented, Space, Tag, Input, Button } from 'antd-mobile';
+import { Divider, Form, Segmented, Space, Tag, Input, Button, Switch } from 'antd-mobile';
 import { S3Config, type UiTheme } from '../services/settings';
 import { useState } from 'react';
 import { EyeInvisibleOutline, EyeOutline } from 'antd-mobile-icons'
@@ -29,99 +29,95 @@ export default function Me({ theme, onThemeChange, s3Config, onS3ConfigChange }:
   }
 
   const testS3Config = async () => {
-    // setS3TestButtonDisabled(true);
-    await s3ConnectTest(s3ConfigState);
-    // try {
-    //   await s3ConnectTest(s3ConfigState);
-    //   showToast('S3 connect success');
-    // } catch (error) {
-    //   showToast(`S3 connect failed: ${error}`);
-    // } finally {
-    //   setS3TestButtonDisabled(false);
-    // }
+    setS3TestButtonDisabled(true);
+    try {
+      await s3ConnectTest(s3ConfigState);
+      showToast('S3 Connect Success');
+    } catch (error) {
+      showToast(`S3 Connect Failed: ${error}`);
+    } finally {
+      setS3TestButtonDisabled(false);
+    }
   }
 
   return (
-    <div style={{ padding: 16 }}>
-      <Space>
-        <Tag color='primary' fill='outline'>开发中</Tag>
-      </Space>
+    <div style={{ padding: 12 }}>
+      <div style={{textAlign: 'center'}}>
+        <Tag color='primary' fill='outline'>Dev</Tag> <span style={{margin: '0 4px'}}></span>
+        <Tag color='primary' fill='outline'>Stat</Tag>
+      </div>
 
       <Divider style={{ borderColor: 'rgba(0, 0, 0, 0)' }}>AWS S3 Configuration</Divider>
 
       {/* s3 配置 */}
-      <Form layout='vertical'>
-        <Form.Item name='accessKeyId' label='accessKeyId' initialValue={s3ConfigState.accessKeyId} extra={
-          // 显示密码可见性切换按钮
-          <div style={{ cursor: 'pointer' }}>
-            {!s3AccessKeyIdVisible ? (
-              <EyeInvisibleOutline onClick={() => setS3AccessKeyIdVisible(true)} />
-            ) : (
-              <EyeOutline onClick={() => setS3AccessKeyIdVisible(false)} />
-            )}
+      <>
+        <Form layout='vertical'>
+          <Form.Item name='AccessKeyId' label='AccessKeyId' initialValue={s3ConfigState.accessKeyId} extra={
+            // 显示密码可见性切换按钮
+            <div style={{ cursor: 'pointer' }}>
+              {!s3AccessKeyIdVisible ? (
+                <EyeInvisibleOutline onClick={() => setS3AccessKeyIdVisible(true)} />
+              ) : (
+                <EyeOutline onClick={() => setS3AccessKeyIdVisible(false)} />
+              )}
+            </div>
+          }>
+            <Input onChange={(next) => setS3Config({ accessKeyId: next })}
+              defaultValue={s3ConfigState.accessKeyId}
+              placeholder='Please input accessKeyId'
+              type={s3AccessKeyIdVisible ? 'text' : 'password'} />
+          </Form.Item>
+          <Form.Item name='SecretAccessKey' label='SecretAccessKey' initialValue={s3ConfigState.secretAccessKey} extra={
+            // 显示密码可见性切换按钮
+            <div style={{ cursor: 'pointer' }}>
+              {!s3SecretVisible ? (
+                <EyeInvisibleOutline onClick={() => setS3SecretVisible(true)} />
+              ) : (
+                <EyeOutline onClick={() => setS3SecretVisible(false)} />
+              )}
+            </div>
+          }>
+            <Input onChange={(next) => setS3Config({ secretAccessKey: next })}
+              defaultValue={s3ConfigState.secretAccessKey}
+              placeholder='Please input secretAccessKey'
+              type={s3SecretVisible ? 'text' : 'password'}
+            />
+          </Form.Item>
+          <Form.Item name='Region' label='Region' initialValue={s3ConfigState.region}>
+            <Input onChange={(next) => setS3Config({ region: next })} defaultValue={s3ConfigState.region} placeholder='Please input region' />
+          </Form.Item>
+          <Form.Item name='Bucket' label='Bucket' initialValue={s3ConfigState.bucket} extra={
+            // 显示密码可见性切换按钮
+            <div style={{ cursor: 'pointer' }}>
+              {!s3BucketVisible ? (
+                <EyeInvisibleOutline onClick={() => setS3BucketVisible(true)} />
+              ) : (
+                <EyeOutline onClick={() => setS3BucketVisible(false)} />
+              )}
+            </div>
+          }>
+            <Input onChange={(next) => setS3Config({ bucket: next })}
+              defaultValue={s3Config.bucket}
+              placeholder='Please input bucket'
+              type={s3BucketVisible ? 'text' : 'password'} />
+          </Form.Item>
+          <Form.Item name='Endpoint' label='Endpoint' initialValue={s3Config.endpoint}>
+            <Input onChange={(next) => setS3Config({ endpoint: next })} defaultValue={s3Config.endpoint} placeholder='Please input endpoint: https://' />
+          </Form.Item>
+        </Form>
+        <Form layout='horizontal' footer={
+          // 测试连接按钮
+          <div style={{ textAlign: 'right' }}>
+            <Button color='primary' fill='outline' onClick={testS3Config} disabled={s3TestButtonDisabled}>
+              Test S3 Connection
+            </Button>
           </div>
         }>
-          <Input onChange={(next) => setS3Config({ accessKeyId: next })} 
-          defaultValue={s3ConfigState.accessKeyId} 
-          placeholder='Please input accessKeyId' 
-          type={s3AccessKeyIdVisible ? 'text' : 'password'} />
-        </Form.Item>
-        <Form.Item name='secretAccessKey' label='secretAccessKey' initialValue={s3ConfigState.secretAccessKey} extra={
-          // 显示密码可见性切换按钮
-          <div style={{ cursor: 'pointer' }}>
-            {!s3SecretVisible ? (
-              <EyeInvisibleOutline onClick={() => setS3SecretVisible(true)} />
-            ) : (
-              <EyeOutline onClick={() => setS3SecretVisible(false)} />
-            )}
-          </div>
-        }>
-          <Input onChange={(next) => setS3Config({ secretAccessKey: next })}
-            defaultValue={s3ConfigState.secretAccessKey}
-            placeholder='Please input secretAccessKey'
-            type={s3SecretVisible ? 'text' : 'password'}
-          />
-        </Form.Item>
-        <Form.Item name='region' label='region' initialValue={s3ConfigState.region}>
-          <Input onChange={(next) => setS3Config({ region: next })} defaultValue={s3ConfigState.region} placeholder='Please input region' />
-        </Form.Item>
-        <Form.Item name='bucket' label='bucket' initialValue={s3ConfigState.bucket} extra={
-          // 显示密码可见性切换按钮
-          <div style={{ cursor: 'pointer' }}>
-            {!s3BucketVisible ? (
-              <EyeInvisibleOutline onClick={() => setS3BucketVisible(true)} />
-            ) : (
-              <EyeOutline onClick={() => setS3BucketVisible(false)} />
-            )}
-          </div>
-        }>
-          <Input onChange={(next) => setS3Config({ bucket: next })} 
-          defaultValue={s3Config.bucket} 
-          placeholder='Please input bucket' 
-          type={s3BucketVisible ? 'text' : 'password'} />
-        </Form.Item>
-        <Form.Item name='endpoint' label='endpoint' initialValue={s3Config.endpoint }>
-          <Input onChange={(next) => setS3Config({ endpoint: next })} defaultValue={s3Config.endpoint} placeholder='Please input endpoint: https://' />
-        </Form.Item>
-        <Form.Item name='forcePathStyle' label='forcePathStyle' initialValue={s3Config.forcePathStyle}>
-          <Segmented
-            options={[
-              { label: 'True', value: 'true' },
-              { label: 'False', value: 'false' },
-            ]}
-            defaultValue={s3Config.forcePathStyle ? 'true' : 'false'}
-            onChange={(next) => {
-              setS3Config({ forcePathStyle: next === 'true' });
-            }}
-          />
-        </Form.Item>
-      </Form>
-
-      <Space wrap>
-          <Button color='primary' fill='solid' onClick={testS3Config} disabled={s3TestButtonDisabled}>
-            Test S3 Connection
-          </Button>
-        </Space>
+          <Form.Item name='ForcePathStyle' label='ForcePathStyle' initialValue={s3Config.forcePathStyle ? 'true' : 'false'} childElementPosition='right'>
+            <Switch defaultChecked={s3Config.forcePathStyle} onChange={(next) => setS3Config({ forcePathStyle: next })} />
+          </Form.Item>
+        </Form>
+      </>
 
       <Divider style={{ borderColor: 'rgba(0, 0, 0, 0)' }}>UI Configuration</Divider>
 
