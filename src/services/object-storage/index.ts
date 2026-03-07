b129f6254ff24735303bce734c31e0ccdbda973b
mdk <578562554@qq.com>
2026-03-07 17:03:00 +0800

    refactor(object-storage): remove unused s3 imports and related code

--- src/services/object-storage/index.ts
+++ src/services/object-storage/index.ts
@@ -1,6 +1,4 @@
 import { ObjectStorage } from "./interface"
 import MemoryObjectStorage from "./memory.fs"
 
-import { S3Config, S3ObjectStorage, s3ConnectTest } from "./s3.fs";
-
 export const ObjectStorageIns: ObjectStorage = new MemoryObjectStorage();
