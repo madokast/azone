fe35cc1ea2f0a891a16d4fea783aea05b0c84b91
zhaorunxiao <zhaorunxiao@sics.ac.cn>
2026-04-10 16:50:15 +0800

    feat(tools): add readableSize function to format file sizes

---
+++ src/tools/readable-size.ts
@@ -0,0 +1,17 @@
+
+// 格式化文件大小
+export function readableSize(size: number) : string {
+  if (size == 0) {
+    return "0";
+  }
+  if (size < 1024) {
+    return `${size} B`;
+  }
+  if (size < 1024 * 1024) {
+    return `${(size / 1024).toFixed(2)} KB`;
+  }
+  if (size < 1024 * 1024 * 1024) {
+    return `${(size / 1024 / 1024).toFixed(2)} MB`;
+  }
+  return `${(size / 1024 / 1024 / 1024).toFixed(2)} GB`;
+}
